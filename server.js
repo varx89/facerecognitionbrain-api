d60b837aa9b1e3c9f9ec28328217abdcd2d031a2
Andrei Varcus <andrei.varcus@yahoo.com>
2022-10-17 23:35:34 +0300

    added connection env elephantsql

--- server.js
+++ server.js
@@ -12,11 +12,8 @@ const image = require('./controllers/image');
 const db = knex({
     client: 'pg',
     connection: {
-      host : '127.0.0.1',
-      port : 5432,
-      user : 'postgres',
-      password : 'groot',
-      database : 'smartbrain'
+      host : process.env.DATABASE_URL,
+      ssl: true,
     }
   });
   
